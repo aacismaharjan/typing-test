d408b8fcbb4eb7804dc5c1c53e71af6f8681a5cf
aacismaharjan <aashishmaharjan2001@gmail.com>
2021-05-22 18:55:51 +0545

    initial commit

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,139 +1,318 @@
-window.addEventListener('load', init);
+import { paragraphs } from "./paragraph.js";
 
-// Globals
-
-// Available Levels
-const levels = {
-  easy: 5,
-  medium: 3,
-  hard: 1
-};
-
-// To change level
-const currentLevel = levels.medium;
-
-let time = currentLevel;
-let score = 0;
-let isPlaying;
+let TYPING_TIME = 0;
+const TYPING_DISPLAY_WORDS = 24;
+let SCORE = 0;
+let distance;
+let wordSpans;
+let startTime;
+let timeRemaining = 0;
+let curWordIndex = 0;
+let curTotalWordIndex = 0;
+let timerInterval = null;
+let displayTimeInterval = null;
+let settings = {};
 
 // DOM Elements
-const wordInput = document.querySelector('#word-input');
-const currentWord = document.querySelector('#current-word');
-const scoreDisplay = document.querySelector('#score');
-const timeDisplay = document.querySelector('#time');
-const message = document.querySelector('#message');
-const seconds = document.querySelector('#seconds');
-const highscoreDisplay = document.querySelector('#highscore');
+const inputWord = document.querySelector("#word-input");
+const paragraphDisplay = document.querySelector("#paragraph");
+const timerDisplay = document.querySelector("#timer");
+const resultDisplay = document.querySelector("#result-screen");
+const previousStatDisplay = document.querySelector("#previous-stats");
+const refreshMatch = document.querySelector("#refresh-match");
+const generalSettingsDisplay = document.querySelector("#general-setting");
+const doneBtn = document.querySelector("#setting-done");
+const settingBtn = document.querySelector("#settingBtn");
 
-const words = [
-  'hat',
-  'river',
-  'lucky',
-  'statue',
-  'generate',
-  'stubborn',
-  'cocktail',
-  'runaway',
-  'joke',
-  'developer',
-  'establishment',
-  'hero',
-  'javascript',
-  'nutrition',
-  'revolver',
-  'echo',
-  'siblings',
-  'investigate',
-  'horrendous',
-  'symptom',
-  'laughter',
-  'magic',
-  'master',
-  'space',
-  'definition'
-];
+// Initialize the game on document load
+window.addEventListener("load", init);
 
-// Initialize Game
-function init() {
-  // Show number of seconds in UI
-  seconds.innerHTML = currentLevel;
-  // Load word from array
-  showWord(words);
-  // Start matching on word input
-  wordInput.addEventListener('input', startMatch);
-  // Call countdown every second
-  setInterval(countdown, 1000);
-  // Check game status
-  setInterval(checkStatus, 50);
+// Refresh match on click
+refreshMatch.addEventListener("click", init);
+
+// Toggle the display of current setting
+settingBtn.addEventListener("click", () => {
+  const gameMode = document.querySelector("#game-mode");
+  const gameTime = document.querySelector("#game-time");
+
+  gameMode.value = settings["gameMode"];
+  gameTime.value = settings["gameTime"];
+  generalSettingsDisplay.classList.toggle("d-none");
+});
+
+// Update the current setting in localStorage
+doneBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const gameMode = document.querySelector("#game-mode");
+  const gameTime = document.querySelector("#game-time");
+
+  localStorage.setItem(
+    "settings",
+    JSON.stringify({
+      gameMode: +gameMode.value,
+      gameTime: +gameTime.value,
+    })
+  );
+
+  generalSettingsDisplay.classList.add("d-none");
+  init();
+});
+
+// Initial function
+export function init() {
+  getSettings();
+
+  SCORE = 0;
+  TYPING_TIME = settings.gameTime * 60;
+  timeRemaining = TYPING_TIME;
+  distance = timeRemaining;
+  startTime = null;
+  curWordIndex = 0;
+  curTotalWordIndex = 0;
+  inputWord.value = "";
+  inputWord.disabled = false;
+  resultDisplay.style.display = "none";
+  previousStatDisplay.style.display = "none";
+  clearInterval(timerInterval);
+  clearInterval(displayTimeInterval);
+  timerInterval = null;
+  displayTimeInterval = null;
+  generalSettingsDisplay.classList.add("d-none");
+
+  // Typing input intial focus
+  inputWord.focus();
+
+  // Inject first paragraph to user
+  const paragraph = paragraphs.getParagraph(settings["gameMode"]);
+  const curParagraph = paragraph
+    .split(/\s+/)
+    .slice(curTotalWordIndex, TYPING_DISPLAY_WORDS)
+    .join(" ");
+
+  injectParagraph(curParagraph);
+
+  // Update timer on valid time distance
+  if (distance >= 0) {
+    updateTimer(distance);
+  }
+
+  // Check Game Over
+  displayTimeInterval = setInterval(function () {
+    const userTotal = paragraphs
+      .getWords(settings["gameMode"])
+      .slice(0, curTotalWordIndex).length;
+    const computerTotal = paragraphs.getWords(settings["gameMode"]).length;
+
+    if (distance <= 0 || userTotal == computerTotal) {
+      GameOver();
+    }
+  }, 100);
+
+  // Detect word and start matching
+  inputWord.addEventListener("input", checkStartMatch);
 }
 
-// Start match
-function startMatch() {
+function checkStartMatch(event) {
+  if (!timerInterval && timeRemaining === TYPING_TIME) {
+    timerInterval = setInterval(function () {
+      if (distance <= 0) {
+        clearInterval(timerInterval);
+      }
+
+      if (distance >= 0) {
+        distance = timeRemaining - 1;
+        updateTimer(distance);
+      }
+
+      timeRemaining -= 1;
+    }, 1000);
+  }
+
+  if (!startTime) startTime = new Date();
+
+  inputWord.value = inputWord.value.trimStart();
+  if (inputWord.value.indexOf(" ") > -1) {
+    curTotalWordIndex++;
+    startMatch(event);
+  }
+}
+
+function startMatch(event) {
+  wordSpans = paragraphDisplay.querySelectorAll("span");
+  wordSpans[curWordIndex].classList.remove("highlight");
+  const curTotal = wordSpans.length;
+
+  if (curWordIndex + 1 >= curTotal) {
+    curWordIndex = 0;
+    injectParagraph(
+      paragraphs
+        .getParagraph(settings["gameMode"])
+        .split(/\s+/)
+        .slice(curTotalWordIndex, curTotalWordIndex + TYPING_DISPLAY_WORDS)
+        .join(" ")
+    );
+    event.target.value = "";
+    wordSpans = paragraphDisplay.querySelectorAll("span");
+    wordSpans[curWordIndex].classList.add("highlight");
+    return;
+  }
+
   if (matchWords()) {
-    isPlaying = true;
-    time = currentLevel + 1;
-    showWord(words);
-    wordInput.value = '';
-    score++;
-  }
-  
-  // Highscore based on score value for Session Storage
-  if (typeof sessionStorage['highscore'] === 'undefined' || score > sessionStorage['highscore']) {
-    sessionStorage['highscore'] = score;
-  } else {
-    sessionStorage['highscore'] = sessionStorage['highscore'];
+    SCORE++;
   }
 
-  // Prevent display of High Score: -1
-  if (sessionStorage['highscore'] >= 0) {
-  highscoreDisplay.innerHTML = sessionStorage['highscore'];
+  if (checkGameOver()) {
+    GameOver();
+    return;
   }
 
-  // If score is -1, display 0
-  if (score === -1) {
-    scoreDisplay.innerHTML = 0;
-  } else {
-    scoreDisplay.innerHTML = score;
-  }
+  event.target.value = "";
+  wordSpans[++curWordIndex].classList.add("highlight");
 }
 
-// Match currentWord to wordInput
 function matchWords() {
-  if (wordInput.value === currentWord.innerHTML) {
-    message.innerHTML = 'Correct!!!';
+  wordSpans = paragraphDisplay.querySelectorAll("span");
+  const wordsX = paragraphDisplay.innerText.split(/\s+/);
+
+  if (wordsX[curWordIndex] === inputWord.value.trim()) {
+    wordSpans[curWordIndex].classList.add("text-success");
     return true;
   } else {
-    message.innerHTML = '';
+    wordSpans[curWordIndex].classList.add("text-danger");
     return false;
   }
 }
 
-// Pick & show random word
-function showWord(words) {
-  // Generate random array index
-  const randIndex = Math.floor(Math.random() * words.length);
-  // Output random word
-  currentWord.innerHTML = words[randIndex];
+function injectParagraph(paragraph) {
+  paragraphDisplay.innerHTML = paragraph
+    .trim()
+    .split(/\s+/)
+    .map((item) => `<span>${item}</span>`)
+    .join(" ");
+  wordSpans = paragraphDisplay.querySelectorAll("span");
+  wordSpans[curWordIndex].classList.add("highlight");
 }
 
-// Countdown timer
-function countdown() {
-  // Make sure time is not run out
-  if (time > 0) {
-    // Decrement
-    time--;
-  } else if (time === 0) {
-    // Game is over
-    isPlaying = false;
+function checkGameOver() {
+  if (timeRemaining <= 0) {
+    return true;
+  } else {
+    return false;
   }
-  // Show time
-  timeDisplay.innerHTML = time;
 }
 
-// Check game status
-function checkStatus() {
-  if (!isPlaying && time === 0) {
-    message.innerHTML = 'Game Over!!!';
-    score = -1;
+// After Game is Over
+function GameOver() {
+  updateGameStats();
+
+  curWordIndex = 0;
+  curTotalWordIndex = 0;
+  inputWord.value = "";
+  inputWord.disabled = true;
+  clearInterval(timerInterval);
+  clearInterval(displayTimeInterval);
+  timerInterval = null;
+  timeRemaining = TYPING_TIME;
+  startTime = null;
+  TYPING_TIME = settings.gameTime * 60;
+}
+
+// Display User Statistics
+function updateGameStats() {
+  resultDisplay.style.display = "flex";
+  generalSettingsDisplay.classList.add("d-none");
+  previousStatDisplay.style.display = "flex";
+  const total = paragraphs
+    .getWords(settings["gameMode"])
+    .slice(0, curTotalWordIndex).length;
+  const seconds = (new Date().getTime() - startTime.getTime()) / 1000;
+  const accuracy = ((SCORE / total) * 100).toFixed(2);
+  const incorrectWords = total - SCORE;
+  const correctWords = SCORE;
+  const wpmGross = Math.floor((total / seconds) * 60);
+
+  const wpmNet = Math.floor((SCORE / seconds) * 60);
+  let highest = +localStorage.getItem("highest");
+  let previousRecords = JSON.parse(localStorage.getItem("records"));
+  let previousRecordsEl;
+
+  const record = {
+    name: "Aashish",
+    accuracy,
+    wpmNet,
+  };
+
+  if (!highest) {
+    localStorage.setItem("highest", wpmNet);
+    highest = wpmNet;
+  } else if (wpmNet > +highest) {
+    localStorage.setItem("highest", wpmNet);
+    highest = wpmNet;
+  }
+
+  if (!previousRecords) {
+    localStorage.setItem("records", JSON.stringify([record]));
+    previousRecordsEl = [record].map((item, index) => getListItem(item, index));
+  } else {
+    localStorage.setItem(
+      "records",
+      JSON.stringify([record, ...previousRecords].slice(0, 5))
+    );
+
+    previousRecordsEl = previousRecords
+      .map((item, index) => getListItem(item, index))
+      .join("");
+  }
+
+  document.querySelector(
+    "#previous-records"
+  ).innerHTML = `<ul class="list-group list-group-flush">${previousRecordsEl}</ul>`;
+
+  // DOM Elements
+  document.querySelector("#accuracy").innerHTML = `${accuracy} %`;
+  document.querySelector(
+    "#incorrectWords"
+  ).innerHTML = `${incorrectWords} Words`;
+  document.querySelector("#correctWords").innerHTML = `${correctWords} Words`;
+  document.querySelector("#wpmGross").innerHTML = `${wpmGross} WPM`;
+  document.querySelector("#wpmNet").innerHTML = `${wpmNet} WPM`;
+  document.querySelector("#highestNetWPM").innerHTML = `${highest} WPM`;
+}
+
+function getListItem(item, index) {
+  return `
+  <li class="list-group-item">
+    <div class="row justify-content-space-between">
+      <div class="col">${index + 1}. ${item.name}</div>
+      <div class="col" id="stat1">
+        ${item.accuracy}%
+      </div>
+      <div class="col" id="stat1">
+        ${item.wpmNet} WPM
+      </div>
+    </div>
+  </li>`;
+}
+
+// distance is in seconds
+function updateTimer(distance) {
+  const minutes = Math.floor(distance / 60);
+  distance = distance % 60;
+  let seconds = Math.floor(distance);
+  if (seconds < 10) seconds = `0${seconds}`;
+  timerDisplay.innerHTML = `${minutes}:${seconds}`;
+}
+
+function getSettings() {
+  if (localStorage.getItem("settings")) {
+    settings = JSON.parse(localStorage.getItem("settings"));
+  } else {
+    settings = {
+      gameMode: 1,
+      gameTime: 1,
+    };
+
+    localStorage.setItem("settings", JSON.stringify(settings));
   }
 }
---
+++ dist/js/paragraph.js
@@ -0,0 +1,21 @@
+const paragraphs = {
+  1: [
+    `this now time set there see was none left right well I we their time sun soon once rock food dark light jump ride car sea dog box bed game table fridge calender class books shoes dress fan chair mother father brother sister sea life  good hog day far wind fish run hope thanks window outdoors tree house school pants shirt brush hat boots pictures phone facebook ring block beach water horse eyes text new fast door add for I'm out of random words to think of this is a simple paragraph made by alexis danyel martin hope you enjoy typing this I guess that's all cause I'm out of ideas I hope you have a very good day or night goodbye`,
+  ],
+  2: [
+    `Days are not of equal value in one's life. Some bring happiness while others bring sadness. Sadness and happiness both are equally important to man's life, since they are the two sides of a coin. As we cannot forget the happiest day, we are unable to forget the saddest day of our life too. The saddest day of my life was the Diwali Day. Diwali is considered to be a happy festival and till last Diwali, it was my favorite festival. On last Diwali, my sister, my brother and I were busy lighting the fireworks. I was holding a ‘fuljhari' in my hand and unfortunately my younger brother, who was standing just beside me, had a cracker in his hand. This cracker caught fire and a very loud explosion was heard which shook my sister and me. After that, we all could think of nothing else than blood stained cotton, bandage, dettol etc. My cousin took my brother to the doctor where he got 14 stitches in his forefinger and thumb. But at home, everybody kept cursing and blaming me for the mishap. That night, I could not sleep and I cried a lot. For next few days, I bore the burden of this blame for being responsible for this unfortunate incident. I felt deeply guilty conscious which I was able to overcome after a long time.`,
+  ],
+  3: [
+    `Recently, an exhibition ‘Building A New India' was held in the capital. It was organized by the Ministry of Information and Broadcasting, Government of India. The exhibition was set up in the Triveni Kala Sangam. The chief exhibits were photographs, novels, some sculptures by Indian modern artists presenting Indian cultural inheritance. First of all, I visited the general section of the exhibition where different charts and photographs depicting India's development in various fields were set. Most impressive photographs among these were those showing India's nuclear development. The second section dealt with India's magnificent historical background. I was fascinated by the pictures of Mohanjodaro excavation. Then I saw the most beautiful and colorful section of the exhibition i.e. the cultural section. It consisted of paintings, sculptures, photographs etc. The Rajasthani and Gujarati paintings were very colourful and attractive. This exhibition, inaugurated by the Prime Minister, lasted for a week. It proved to be of great educational value. It brushed up my knowledge about India as my motherland. It enhanced my respect for my great country, India. I would very much appreciate if the Indian government organized some more such exhibitions.`,
+  ],
+  getParagraph: function (mode) {
+    const index = Math.floor(Math.random() * this[mode].length);
+    return this[mode][index].replace(/(\r\n|\r|\n){2,}/g, "$1\n");
+  },
+
+  getWords: function (mode) {
+    return this.getParagraph(mode).trim().split(/\s+/);
+  },
+};
+
+export { paragraphs };
