c886a6a06a2abbba49bd2e34f0bfa7d6cb9a5710
aacismaharjan <aashishmaharjan2001@gmail.com>
2021-06-02 11:20:26 +0545

    [Feat] Display word status

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,126 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Sidenav from "./components/Sidenav/Sidenav";
+import Header from "./components/Header/Header";
+import { Container } from "react-bootstrap";
+
+const StyledContent = styled.div`
+  margin-left: 150px;
+`;
+
+const StyledApp = styled.div`
+  min-height: 100vh;
+  background: rgba(52, 58, 64, 0.94);
+`;
+
+const text = `Days are not of equal value in one's life. Some bring happiness
+            while others bring sadness. Sadness and happiness both are equally
+            important to`;
+
+const WordSpan = (props: any) => {
+  const getClassName = () => {
+    if (props.index === props.counter) {
+      return "highlight";
+    } else if (props.index < props.counter) {
+      console.log(props.text, props.words, props.index);
+      if (props.text[props.index] === props.words[props.index]) {
+        console.log("BINGO");
+        return "text-success";
+      } else {
+        return "text-danger";
+      }
+    }
+
+    return "";
+  };
+
+  return (
+    <React.Fragment>
+      <span className={getClassName()}>{props.children}</span>{" "}
+    </React.Fragment>
+  );
+};
+
+const TypingTextWords = (props: any) => {
+  return (
+    <p className="lead p-3 bg-white text-secondary rounded">
+      {props.text.split(/\s+/).map((word: string, index: number) => (
+        <WordSpan
+          key={index}
+          index={index}
+          words={props.words}
+          text={props.text.split(/\s+/)}
+          counter={props.counter}
+          curWord={props.word}
+        >
+          {word}
+        </WordSpan>
+      ))}
+    </p>
+  );
+};
+
+const TypingTextInput = (props: any) => {
+  const handleChange = (event: any) => {
+    const word = event.target.value;
+
+    if (word.indexOf(" ") > -1) {
+      props.setValue(word);
+      event.target.value = "";
+    }
+  };
+
+  return (
+    <div className="row no-gutters">
+      <div className="col">
+        <input
+          type="text"
+          className="form-control rounded-left py-1"
+          id="word-input"
+          autoFocus
+          autoComplete="off"
+          autoCapitalize="off"
+          autoCorrect="off"
+          style={{ fontSize: "1.25rem" }}
+          onChange={handleChange}
+        />
+      </div>
+    </div>
+  );
+};
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [word, setWord] = useState("");
+  const [words, setWords] = useState<string[]>([]);
+
+  useEffect(() => {
+    if (word) {
+      setCounter((couter) => couter + 1);
+      setWords((words) => [...words, word.trim()]);
+    }
+  }, [word]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledApp>
+      <Sidenav />
+
+      <StyledContent>
+        <Header />
+
+        <Container style={{ paddingTop: "30px" }}>
+          <TypingTextWords
+            text={text}
+            counter={counter}
+            word={word}
+            words={words}
+          />
+          <TypingTextInput setValue={setWord} />
+        </Container>
+      </StyledContent>
+    </StyledApp>
   );
 }
 
---
+++ src/components/Sidenav/Sidenav.tsx
@@ -0,0 +1,90 @@
+import React from "react";
+import { Nav } from "react-bootstrap";
+import { FiPlay, FiSettings } from "react-icons/fi";
+import { BsGraphUp } from "react-icons/bs";
+import styled from "styled-components";
+import { IconContext } from "react-icons/lib";
+import { LinkContainer } from "react-router-bootstrap";
+
+const StyledSidenav = styled.div`
+  height: 100%;
+  width: 150px;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  background-color: #32373c;
+  overflow-x: hidden;
+  padding-top: 20px;
+
+  .nav-container {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    .nav-item {
+      width: 100%;
+      color: white;
+      flex: 1;
+      flex-direction: column;
+      height: 100%;
+      max-height: 120px;
+      gap: 15px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      &:hover {
+        background-color: rgba(0, 0, 0, 0.15);
+      }
+
+      &.active,
+      &.active:hover {
+        color: #fff;
+        text-decoration: none;
+        background-color: #007bff;
+      }
+    }
+  }
+`;
+
+const Sidenav = () => {
+  return (
+    <StyledSidenav>
+      <Nav className="nav-container flex-column">
+        <LinkContainer to="/start-typing">
+          <Nav.Link className="nav-item">
+            <IconContext.Provider value={{ size: "2rem" }}>
+              <FiPlay />
+            </IconContext.Provider>
+            Start Typing
+          </Nav.Link>
+        </LinkContainer>
+
+        <LinkContainer to="/game-stats">
+          <Nav.Link className="nav-item">
+            <IconContext.Provider value={{ size: "1.8rem" }}>
+              <BsGraphUp />
+            </IconContext.Provider>
+            Game Stats
+          </Nav.Link>
+        </LinkContainer>
+
+        <LinkContainer to="/settings">
+          <Nav.Link className="nav-item">
+            <IconContext.Provider value={{ size: "2rem" }}>
+              <FiSettings />
+            </IconContext.Provider>
+            Settings
+          </Nav.Link>
+        </LinkContainer>
+      </Nav>
+    </StyledSidenav>
+  );
+};
+export default Sidenav;
